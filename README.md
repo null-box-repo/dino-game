81e623a4273ca026edf17e4d83e07882268f31a7
YasserNull <yasserbnull@gmail.com>
2026-08-31 07:14:07 +0100

    .

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# dino-game
-https://github.com/SSSDNSY/FREE-SOURCE-CODE/blob/master/dino-game.html#L
+# Dino Game
+![[logo](icon.jpg)
+The classic Chrome Dino Game that appears when offline.
+<p align="center">
+  <img src="https://img.shields.io/github/downloads/null-box-repo/dino-game/total?label=Downloads"/>
+  <img src="https://img.shields.io/github/v/release/null-box-repo/dino-game?include_prereleases&label=Release"/>
+  <img src="https://img.shields.io/badge/License-BSD--3--Clause-blue.svg"/>
+</p>
+
+![screenshot](docs/images/screenshot.jpg)
+
+dino-game.html source: https://github.com/SSSDNSY/FREE-SOURCE-CODE/blob/master/dino-game.html
--- setup.sh
+++ setup.sh
@@ -6,19 +6,19 @@ YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
-echo "${BLUE}[1/3] Updating packages...${NC}"
-apk update && echo "${GREEN}[1/3] Update complete${NC}"
+echo -e "${BLUE}[1/3] Updating packages...${NC}"
+apk update && echo -e "${GREEN}[1/3] Update complete${NC}"
 
-echo "${BLUE}[2/3] Upgrading packages...${NC}"
-apk upgrade && echo "${GREEN}[2/3] Upgrade complete${NC}"
+echo -e "${BLUE}[2/3] Upgrading packages...${NC}"
+apk upgrade && echo -e "${GREEN}[2/3] Upgrade complete${NC}"
 
-echo "${BLUE}[3/3] Checking PHP...${NC}"
+echo -e "${BLUE}[3/3] Checking PHP...${NC}"
 if command -v php &> /dev/null; then
-    echo "${GREEN}[3/3] PHP is already installed${NC}"
+    echo -e "${GREEN}[3/3] PHP is already installed${NC}"
 else
-    echo "${YELLOW}[3/3] PHP not found${NC}"
-    echo "${BLUE}[3/3] Installing PHP...${NC}"
-    apk add php && echo "${GREEN}[3/3] PHP installed successfully${NC}"
+    echo -e "${YELLOW}[3/3] PHP not found${NC}"
+    echo -e "${BLUE}[3/3] Installing PHP...${NC}"
+    apk add php && echo -e "${GREEN}[3/3] PHP installed successfully${NC}"
 fi
 
-echo "${GREEN}All done${NC}"
+echo -e "${GREEN}All done${NC}"
